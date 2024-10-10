0ff40c0a1087d7ae4c49e98e38e3b71ad69ee8d4
Codingtime <13tia.vora@dulwich-seoul.kr>
2024-10-10 18:46:13 +0900

    car or bike

---
+++ ride.py
@@ -0,0 +1,14 @@
+ask = input("What is prefrence in vehicle, car or bike?")
+if ask == "bike":
+    bike = input("Do you like motorbike or scooter?")
+    if bike == "motorbike":
+          print("Wow, fast!")
+    else:
+          print('Good choice!')
+
+else:
+        car = input("Do you like sports car or electric car?")
+        if car == "electric":
+          print("Wow, Sustainable!")
+        else:
+          print('You are a fast driver!!!')
